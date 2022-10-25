e4c17eeabd7d41d6c575ca84ccc5574f5c2a177a
Roman Stakhiv <roman.stakhiv@inoxoft.com>
2022-10-25 18:05:42 +0300

    test: eslint rule

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { makeStyles } from 'tss-react/mui';
 import { Link } from '@/components/link/Link';
 
