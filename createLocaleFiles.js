91b0e045128871be94c66f070e7bcc6ab8084c14
Roman Stakhiv <roman.stakhiv@inoxoft.com>
2022-10-25 17:57:25 +0300

    refactor: add error handling for pere build script

--- createLocaleFiles.js
+++ createLocaleFiles.js
@@ -1,30 +1,47 @@
 const fs = require('fs');
 const dir = './public/locales/es';
 
-fetch('https://dev-be.creativesociety.com/api/v1/translations/langs').then(
-  (res) => {
-    res.json().then((data) => {
-      console.log(data.results);
-      fs.writeFileSync(
-        './next-i18next.config.js',
-        `module.exports = {
+fetch('https://dev-be.creativesociety.com/api/v1/translations/langs')
+  .then((res) => {
+    res
+      .json()
+      .then((data) => {
+        fs.writeFileSync(
+          './next-i18next.config.js',
+          `module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: [${data.results.map(({ code }) => `'${code}'`)}],
   },
 };`,
-      );
-    });
-  },
-);
+        );
+
+        console.log(' ✅ Successfully created next-i18next.config.js');
+      })
+      .catch((err) => {
+        console.error(
+          '❌ Problem with creation next-i18next.config.js file',
+          err,
+        );
+      });
+  })
+  .catch((err) => {
+    console.log(err.response.data);
+  });
 
 fetch('https://dev-be.creativesociety.com/api/v1/translations?lang=ru').then(
   (res) => {
-    res.json().then((data) => {
-      fs.rm(dir, { recursive: true, force: true }, () => {
-        fs.mkdirSync(dir);
-        fs.writeFileSync(`${dir}/common.json`, JSON.stringify(data));
+    res
+      .json()
+      .then((data) => {
+        fs.rm(dir, { recursive: true, force: true }, () => {
+          fs.mkdirSync(dir);
+          fs.writeFileSync(`${dir}/common.json`, JSON.stringify(data));
+        });
+        console.log(' ✅ Successfully created locales');
+      })
+      .catch((err) => {
+        console.log('❌', err.response.data);
       });
-    });
   },
 );
