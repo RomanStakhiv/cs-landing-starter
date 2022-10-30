73e5e54b8bdcf1e2f3c93a3b06d33b343cfabb13
Roman Stakhiv <roman.stakhiv@inoxoft.com>
2022-10-30 10:46:28 +0200

    build: crate new build

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'next-i18next';
 import { getStaticPaths, makeStaticProps } from '@src/lib/getStatic';
-import Image from 'next/image';
 import { Box } from '@mui/material';
 import React from 'react';
 import { makeStyles } from 'tss-react/mui';
@@ -9,10 +8,10 @@ import { MainLayout } from '@cmp/layout/mainLayout/MainLayout';
 const useStyles = makeStyles()((theme) => ({
   box: {
     position: 'relative',
-    height: '200px',
+    height: 'auto',
 
     [theme.breakpoints.up('md')]: {
-      height: '500px',
+      height: 'auto',
     },
   },
 }));
@@ -24,10 +23,9 @@ const Home = () => {
   return (
     <MainLayout>
       <Box className={classes.box}>
-        <Image
+        <img
           src="/assets/images/circle-background.png"
           alt="Picture of the author"
-          fill
         />
       </Box>
       <h1>{t('More details')}</h1>
